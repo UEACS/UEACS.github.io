22a7da0f61dd882dca5474a4f2105f0c4198d18b
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-03-10 00:19:17 +0000

    Added dark mode slider
    Dark mode slider doesn't use cookies so always forgets if you put it in dark mode.

--- themes.js
+++ themes.js
@@ -1,14 +1,18 @@
 function onThemeSwitch()
 {
     console.log('clicked');
-    if(cssLink.href == "style.css")
+    console.log(cssLink.href.substring(cssLink.href.lastIndexOf('/') + 1));
+    if(cssLink.href.substring(cssLink.href.lastIndexOf('/') + 1) === "style%20light.css")
     {
         cssLink.href = "style dark.css";
+        console.log('light theme changed to dark theme');
     }
     else
     {
-        cssLink.href = "style.css";
+        cssLink.href = "style light.css";
+        console.log('dark theme changed to light theme');
     }
+    console.log('switched');
 }
 
 const cssLink = document.head.querySelector('link');

f4c708e4248256bb351851eeb6b296f4651387de
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-03-15 09:48:29 +0000

    Theme syncs between all pages on the same device

--- themes.js
+++ themes.js
@@ -6,15 +6,21 @@ function onThemeSwitch()
     {
         cssLink.href = "style dark.css";
         console.log('light theme changed to dark theme');
+        localStorage.setItem('theme','dark');
     }
     else
     {
         cssLink.href = "style light.css";
         console.log('dark theme changed to light theme');
+        localStorage.setItem('theme','light');
     }
     console.log('switched');
 }
 
 const cssLink = document.head.querySelector('link');
 const themeSwitch = document.querySelector('#theme-switch');
+if (localStorage.getItem('theme')=='dark')
+{
+    onThemeSwitch();
+}
 themeSwitch.addEventListener("click", onThemeSwitch);

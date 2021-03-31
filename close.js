045d80fea94e23428aa8adfebb7252d7e017159a
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-03-31 20:06:10 +0100

    Added Cookie Notice

--- close.js
+++ close.js
@@ -1,14 +1,18 @@
 function closeParent()
 {
     console.log('clicked');
-    const box = document.querySelector('#floating-notice');
     box.remove();
     console.log('closed');
     localStorage.setItem('noticed','true');
 }
 
-closeParent(); // When election is about to begin, remove this line
+//localStorage.setItem('noticed','false');
+//closeParent(); // When election is about to begin, remove this line
 const cross = document.querySelector('#notice-cross');
+const box = document.querySelector('#floating-notice');
+box.querySelector("strong").innerHTML = "We don't respect your privacy";
+box.querySelector("p").innerHTML = "<h4>Like, at all. Now accept my cookies bitch.</h4><h5>Also I cba to make an accept buttons so just click the cross and ignore this.</h5><h6>(There's only 2 cookies, one for saving which theme you set and the other is literally just so you don't see this message again!)</h6>";
+
 if (localStorage.getItem('noticed')=='true')
 {
     closeParent();

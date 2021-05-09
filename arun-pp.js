9b958905fcb7c2bbfca4515e83824ce9bef1b778
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-05-09 17:56:18 +0100

    Small update to history and css for unordered list improved
    All pps updated.

--- arun-pp.js
+++ arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/7d4c7c034912d1bf34d86763125f0799.webp?size=128";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/72490d3f18dafda1528ad68fa421d1dc.webp?size=128";

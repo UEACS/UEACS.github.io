68d85d7b49b7f6f4e0f3b347f47eb422c84708da
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-06-10 13:48:45 +0100

    Simplified light theme, and updated profile pictures
    Re-organised files into media files. Made channel marks the right colour for dark theme.

--- arun-pp.js
+++ arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/72490d3f18dafda1528ad68fa421d1dc.webp?size=128";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/ec06660078d95b05e724c3230819d9b5.webp?size=128";

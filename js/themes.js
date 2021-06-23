6541101c6229615eccf02ee8ef770927eb76dfe0
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-06-23 12:50:17 +0100

    CSS and JS organised into folders and minor css changes
    The navigation bar is now smaller when at the top of the page.

--- js/themes.js
+++ js/themes.js
@@ -2,15 +2,15 @@ function onThemeSwitch()
 {
     console.log('clicked');
     console.log(cssLink.href.substring(cssLink.href.lastIndexOf('/') + 1));
-    if(cssLink.href.substring(cssLink.href.lastIndexOf('/') + 1) === "style.css")
+    if(cssLink.href.substring(cssLink.href.lastIndexOf('/') + 1) === "style%20light.css")
     {
-        cssLink.href = "style dark.css";
+        cssLink.href = "./css/style dark.css";
         console.log('light theme changed to dark theme');
         localStorage.setItem('theme','dark');
     }
     else
     {
-        cssLink.href = "style.css";
+        cssLink.href = "./css/style light.css";
         console.log('dark theme changed to light theme');
         localStorage.setItem('theme','light');
     }

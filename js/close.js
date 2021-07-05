84b5e53d884e7facaebb2ed02c89ea9fd8e7419d
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-07-05 03:42:54 +0100

    Election end date added to manifesto page and notice. Notice recouloured and manifesto link added

--- js/close.js
+++ js/close.js
@@ -6,12 +6,16 @@ function closeParent()
     localStorage.setItem('noticed','July Election');
 }
 
-//localStorage.setItem('noticed','false');
+localStorage.setItem('noticed','false');
 //closeParent(); // When election is about to begin, remove this line
 const cross = document.querySelector('#notice-cross');
 const box = document.querySelector('#floating-notice');
-box.querySelector("strong").innerHTML = " BREAKING NEWS: New Election Coming Soon!";
+box.querySelector("strong").innerHTML = "JULY ELECTION: Voting ends Saturday";
 box.querySelector("p").innerHTML = "Stay up to date with election information, candidate statements and our own takes and insights into the election.";
+var link = document.createElement("a");
+link.href="manifestos.html";
+link.innerHTML="<p>Manifestos</p>";
+box.appendChild(link);
 
 if (localStorage.getItem('noticed')=='July Election')
 {

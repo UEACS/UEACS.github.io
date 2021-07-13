e7a1cbe97a01a7ffcf436b55834bc31bba493280
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-07-13 23:04:36 +0100

    Names can be added and will be used. Cards updated with this change and some minor wording fixes

--- js/drinkage.js
+++ js/drinkage.js
@@ -1,27 +1,3 @@
-function nextCard()
-{
-    console.log("Next card please");
-    if (Math.random()>0.3)
-    {
-        card.querySelector("p").innerHTML = common[Math.floor(Math.random()*common.length)];
-    }
-    else
-    {
-        var index = Math.floor(Math.random()*rare.length);
-        card.querySelector("p").innerHTML = rare[index];
-        rare.splice(index,1);
-        console.log(rare);
-    }
-    
-}
-
-
-const nextButton = document.querySelector('#next-button');
-const card = document.querySelector('.card');
-card.querySelector("p").innerHTML = "Welcome To Drinkage!";
-
-var text = "No cards found!";
-
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
@@ -39,6 +15,63 @@ function readTextFile(file)
     }
     rawFile.send(null);
 }
+
+function nextCard()
+{
+    console.log("Next card please");
+    if (Math.random()>0)
+    {
+        // Common cards
+        instruction = common[Math.floor(Math.random()*common.length)];
+        
+        instruction = instruction.replace('prsn',names[Math.floor(Math.random()*names.length)]) // Randomly put in players names to suitable marked positions
+        card.querySelector("p").innerHTML = instruction;
+    }
+    else
+    {
+        // Rare cards
+        var index = Math.floor(Math.random()*rare.length);
+        instruction = rare[index];
+        instruction = instruction.replace("prsn",names[Math.floor(Math.random()*names.length)]) // Randomly put in players names to suitable marked positions
+        card.querySelector("p").innerHTML = instruction;
+        rare.splice(index,1);
+    }
+    
+}
+
+function addName()
+{
+    // Gets name value and clears field
+    console.log("Adding player");
+    var name = nameInput.value;
+    if (name != "")
+    {
+        nameInput.value = "";
+        console.log(name);
+
+        // Adds name to list and makes a new player card
+        names.push(name);
+        var nameCard = document.createElement('div');
+        nameCard.classList.value = "button";
+        nameCard.innerHTML = name;
+
+        namesContainer.appendChild(nameCard);
+    }
+}
+
+
+const nextButton = document.querySelector('#next-button');
+const card = document.querySelector('.card');
+const nameInput = document.querySelector('#name');
+const namesContainer = document.querySelector('#names');
+
+var instruction
+var names = [];
+card.querySelector("p").innerHTML = "Welcome To Drinkage!";
+
+var text = "No cards found!";
+
+
 readTextFile("https://ueacs.co.uk/js/drinkCom.txt");
 
 console.log("Common:"+text);

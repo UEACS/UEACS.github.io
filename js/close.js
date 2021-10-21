aff6baee849801f28829bb634b762c16f5eff25b
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-10-21 12:20:05 +0100

    Mention of a "Ben Smith"? removed from introduction and new discord changes alert added

--- js/close.js
+++ js/close.js
@@ -3,7 +3,7 @@ function closeParent()
     console.log('clicked');
     box.remove();
     console.log('closed');
-    localStorage.setItem('noticed','No cookies');
+    localStorage.setItem('noticed','Server changes 2021');
 }
 
 const cross = document.querySelector('#notice-cross');
@@ -11,14 +11,14 @@ const box = document.querySelector('#floating-notice');
 //localStorage.setItem('noticed','false');
 //closeParent(); // When election is about to begin, remove this line
 
-if (localStorage.getItem('noticed')=='No cookies')
+if (localStorage.getItem('noticed')=='Server changes 2021')
 {
     closeParent();
 }
 else
 {
-    box.querySelector("strong").innerHTML = "We don't collect cookies";
-    box.querySelector("p").innerHTML = "Just to let you know";
+    box.querySelector("strong").innerHTML = "WARNING";
+    box.querySelector("p").innerHTML = "The discord server will be getting a big overhaul soon with changes to channels, organisation and policies. Many of these suggestions are being made by Jack Mawer so watch out. The admin team will likely implement the agreed upon changes this weekend!";
     //var link = document.createElement("a");
     // link.href="admins.html";
     // link.innerHTML="<p>New Admins</p>";

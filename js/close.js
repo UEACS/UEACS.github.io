37910627ad209586e41d47838ce28e0a30801a97
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-12-10 18:51:12 +0000

    New feedback notice

--- js/close.js
+++ js/close.js
@@ -3,7 +3,7 @@ function closeParent()
     console.log('clicked');
     box.remove();
     console.log('closed');
-    localStorage.setItem('noticed','New admins');
+    localStorage.setItem('noticed','Racism');
 }
 
 const cross = document.querySelector('#notice-cross');
@@ -11,14 +11,14 @@ const box = document.querySelector('#floating-notice');
 //localStorage.setItem('noticed','false');
 //closeParent(); // When election is about to begin, remove this line
 
-if (localStorage.getItem('noticed')=='New admins')
+if (localStorage.getItem('noticed')=='Racism')
 {
     closeParent();
 }
 else
 {
-    box.querySelector("strong").innerHTML = "New Admins Selected";
-    box.querySelector("p").innerHTML = "Arvy, Lukasz and Charlie have joined the admin team!";
+    box.querySelector("strong").innerHTML = "Feedback";
+    box.querySelector("p").innerHTML = "If you have any issues regarding how the server is ran, please talk to the admin team. Arun, Charis, Arvy and Jack are the most responsible but we are all here to help!";
     //var link = document.createElement("a");
     // link.href="admins.html";
     // link.innerHTML="<p>New Admins</p>";

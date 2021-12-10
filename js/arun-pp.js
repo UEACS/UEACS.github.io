c21526e46687b58e0e793558b8a8477518ab7495
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-12-10 18:47:03 +0000

    Made many updates to admin page including adding Arvy, Jugger and Lukasz

--- js/arun-pp.js
+++ js/arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/13ad2daa3d83851ced7891e435356cc0.webp?size=80";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/dc6fdde7b5c26e25bfc94915d1ddc8cb.webp?size=80";

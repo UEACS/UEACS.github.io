02c23dab05ffb97d435e28929c7246321c48b89e
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2022-03-01 18:09:06 +0000

    Removed manifesto page and updated admin profile pictures

--- js/arun-pp.js
+++ js/arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/f39b7d0ff9e8e159de1e714e4e2d4348.webp?size=80";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/a_0110f7b3ff1dc940a062d98b92b7dc55.gif?size=80";

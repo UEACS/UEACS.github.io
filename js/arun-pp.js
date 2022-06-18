836f005a92b50e728915df0ecd0410a938816c54
Arun Osborn <arunosborn@gmail.com>
2022-06-18 15:19:27 +0100

    Fixed floating notice

--- js/arun-pp.js
+++ js/arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/7f902ef3979e0c2d04f64cf4681f8d85.webp?size=80";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/80c018fe1e7df2462ab81d66802a7336.webp?size=80";

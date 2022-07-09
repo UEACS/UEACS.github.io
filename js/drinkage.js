24578b4b7be698896c123b864d4683356264e4eb
Arun Osborn <arunosborn@gmail.com>
2022-07-09 16:17:58 +0100

    Revert "Fixed merge issues"
    This reverts commit f8f8c553d0bc281affa33851382bc368956101d7.

--- js/drinkage.js
+++ js/drinkage.js
@@ -87,7 +87,7 @@ function nextCard()
         {
             if (multipleMarkedPeople.length == 1)
             {
-                instruction = `<b>${multipleMarkedPeople[0]}</b> was selected multiple times so must down their drinks!`;
+                instruction = `<b>${multipleMarkedPeople[0]}</b> was selected multiple times so must down their drink!`;
             }
             else
             {
@@ -96,7 +96,7 @@ function nextCard()
                 {
                     instruction += `<b>${person}</b><br>`;
                 }*/
-                instruction += " were selected multiple times so must down their drink!";
+                instruction += " were selected multiple times so must down their drinks!";
             }
         }
         else

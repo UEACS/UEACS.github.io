6105cca859a3a4e3eadb0aa0c4b86b2d2643c2fa
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2023-03-30 14:21:57 +0100

    Updated arun pp

--- js/arun-pp.js
+++ js/arun-pp.js
@@ -1,2 +1,2 @@
 var pp = document.querySelector(".arun-pp");
-pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/33f419a117726da9c20ecc761d9b4a8b.webp?size=80";
+pp.src = "https://cdn.discordapp.com/avatars/258284765776576512/2cd6c9f7e5a6a92dea333e14668f093f.webp?size=160";
